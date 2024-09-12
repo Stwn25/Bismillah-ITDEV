1f016f83082c6f74fdfb6ab7c2f31d4afedd3c51
Raffiefh <ifranraffi24@gmail.com>
2024-09-12 16:26:52 +0700

    Perubahan dikit dikit

--- js/katalog.js
+++ js/katalog.js
@@ -9,26 +9,29 @@ function getProduct() {
     .then((data) => {
         var container = document.querySelector("#product_container");
         data.forEach((product) => {
-        product.description = shortText(product.description, 70);
-        product.title = shortText(product.title, 20);
-        container.insertAdjacentHTML("beforeend",
-            `<div class="card">
+        product.description = shortText(product.description, 100);
+        product.title = shortText(product.title, 50);
+        container.insertAdjacentHTML(
+          "beforeend",
+          `<div class="card">
                         <div class="card-img">
                             <img src="${product.image}"
                                 alt="img product">
                         </div>
                         <section>
-                            <header>
-                                <p class="fw-bold">${product.title}</p>
-                            </header>
+                                                    <p class="fw-bold">${
+                                                      product.title
+                                                    }</p>
                             <p>${product.description}</p>
-                            
+                            <p>${product.description}</p>
+                            <p>Kategori: ${product.category}</p>
+                            <p>Harga: $${product.price}</p>
                             <a href="detail/detail.html?id=${product.id}">
                                 <button class="bg-blue p-2 text-white fw-bold border-radius shadow">detail</button>
                             </a>
                         </section>
                     </div>`
-                );
+        );
             });
         });
 }
